a64caf7f30d0a2c966cb0c7dbd7b29fa241e8ab9
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2026-07-20 12:01:58 +0600

    feat: add version 2

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "Student Grade Calculator\n";
+    cout << "Student Grade Calculator V2\n";
     return 0;
 }
 double calculateAverage(int a, int b, int c) {
